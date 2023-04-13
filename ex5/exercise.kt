428612d155cdf493c60a91bfbeed40fdc8c8191a
VasiFran <127098109+VasiFran@users.noreply.github.com>
2023-04-13 11:19:45 +0200

    Added Kotlin Exercise 5

--- ex5/exercise.kt
+++ ex5/exercise.kt
@@ -1,7 +1,14 @@
 package Exercises.KotlinExercises.ex5
 
-import kotlin.collections.List as List1
-
 fun main(){
     val valueList = listOf(10, 564, 23, 20, 9000, 34, 69, 90, 420, 777)
+    valueList.filterSmaller(400).printValues()
+}
+
+fun List<Int>.filterSmaller(numFilt: Int): List<Int> {
+    return this.filter{it < numFilt}
+}
+
+fun List<Int>.printValues(){
+    forEach{println(it)}
 }
